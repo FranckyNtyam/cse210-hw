69252b06d30d3d8ef86888404fc0f9d82cd910da
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-09-14 00:20:43 +0100

    Write a program that determines the letter grade for a course

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,64 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Enter your grade percentage");
+        string grade_percentage = Console.ReadLine();
+        int number = int.Parse(grade_percentage);
+        string letter;
+        int remainder = number%10;
+        string sign;
+        if (number >= 90)
+        {
+            letter = "A";
+        }
+         else if (number >= 80)
+        {
+            letter = "B";
+        }
+         else if (number >= 70)
+        {
+            letter = "C";
+        }
+         else if (number >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+        if (remainder >= 7 && number >= 90)
+        {
+            sign = "";
+        }
+        else if (remainder < 3 && number >= 90)
+        {
+            sign = "-";
+        }
+        else if (number < 60 )
+        {
+            sign = "";
+        }
+        else if (remainder >= 7)
+        {
+            sign = "+";
+        }
+        else if (remainder < 3)
+        {
+            sign = "-";
+        }
+        
+        else
+        {
+            sign = "";
+        }
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+        if (number >= 70)
+        {
+            Console.WriteLine("Congratulation you passed your examen.");
+        }
+        else
+        {Console.WriteLine("Never give up! go ahead");
+        }
     }
 }
