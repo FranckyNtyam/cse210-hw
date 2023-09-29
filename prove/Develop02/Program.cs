dc8ebab674f4462dd8f2e6d1d816be5e0cd6befb
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-09-29 17:44:19 +0100

    Update project with saving option

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,56 +1,68 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics.Metrics;
+using System.Formats.Asn1;
+using System.IO;
 
-class Program
-{
-    static void Main(string[] args)
+PromptGenerator prompt = new PromptGenerator();
+Journal theJournal = new Journal();
+Entry entries = new Entry();
+DateTime date = DateTime.Now;
+entries._date = date;
+entries._promptText = prompt.GetRandomPrompt();
+entries._entryText = "";
+int menuSelect = 0;
+
+
+Console.WriteLine("Welcome to the Journal Program!");
+   do
+   { 
+    Console.WriteLine("Please select one of the following choices:");
+    Console.WriteLine("1. Write");
+    Console.WriteLine("2. Display");
+    Console.WriteLine("3. Load");
+    Console.WriteLine("4. Save");
+    Console.WriteLine("5. Quit");
+    Console.Write("What would you like to do? ");
+    string selectMenu = Console.ReadLine();
+    menuSelect = int.Parse(selectMenu);
+   
+
+    if (menuSelect == 1)
     {
-        PromptGenerator prompt = new PromptGenerator();
-        Journal theJournal = new Journal();
-        Entry entries = new Entry();
-        DateTime date = DateTime.Now;
-        entries._date = date;
-        entries._promptText = prompt.GetRandomPrompt();
-        entries._entryText = "";
-        int menuSelect = 0;
-        
-
-        Console.WriteLine();
-        do
-        {Console.WriteLine("Please select one of the following choices:");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.Write("What would you like to do? ");
-        string selectMenu = Console.ReadLine();
-        menuSelect = int.Parse(selectMenu);
-
-        if (menuSelect == 1)
-        {
-           Console.WriteLine(entries._promptText);
-           entries._entryText = Console.ReadLine();
-        }
-            
-           
+        Console.WriteLine(entries._promptText);
+        entries._entryText = Console.ReadLine();
         theJournal.AddEntry(entries.Display());
-
-        if (menuSelect == 2)
-        {
-           theJournal.Display();
-        }
-         theJournal.counter++;
-        }
-        while (menuSelect < 5);
-        
-      
-        
-        
-       
-       
-        
-         
     }
-}
+    if (menuSelect == 2)
+    {
+        
+        theJournal.Display();
+    }
+    if ( menuSelect == 3)
+    {
+         Console.WriteLine("What is the filename ");
+        string file = Console.ReadLine();
+        theJournal.LoadFromFile(file);
+       
+    }
+
+    if ( menuSelect == 4)
+    {
+        Console.WriteLine("What is the filename ");
+        string file = Console.ReadLine();
+        theJournal.SaveToFile(file);
+        
+    }
+
+      theJournal.counter++;
+    }
+    while (menuSelect < 5);
+    
+
+
+
+
+
+
+
--- prove/Develop02/journal.cs
+++ prove/Develop02/journal.cs
@@ -1,27 +1,52 @@
-
+using System.IO;
 public class Journal
 {
     public List<string> _entries = new List<string>();
-    public int counter;
-   
-    
 
+    public int counter;
+    public string[] arrayLine;
+   
+   DateTime date = DateTime.Now;
+     
+    
 
     public void AddEntry(string newEntry)
     {
+       
         _entries.Insert(counter, newEntry);
          
     }
 
     public void Display()
     {
-        for (int i = 0; i < _entries.Count-1; i++)
+        for (int i = 0; i < _entries.Count; i++)
         {
           Console.WriteLine(_entries[i]);
         }
     }
     public void SaveToFile(string file)
-    {}
+    {
+        using (StreamWriter writer = new StreamWriter(file, true))
+        {
+            
+             for (int i = 0; i < _entries.Count; i++)
+        {
+          writer.WriteLine(_entries[i]);
+        }
+        }
+
+    }
     public void LoadFromFile(string file)
-    {}
+    {
+       string[] entries = System.IO.File.ReadAllLines(file);
+       
+        
+        foreach (string line in entries)
+       {
+         arrayLine = line.Split(".");
+    
+       }
+        _entries = arrayLine.ToList();
+
+    }
 }
