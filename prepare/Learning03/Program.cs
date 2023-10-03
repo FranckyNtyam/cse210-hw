cde1a9895d10a8c19164d25d53c740b4d461965d
Ntyam Adjomo Francky Ludovic <francky.ntyam@gmail.com>
2023-10-03 14:45:46 +0100

    Encapsulation learning activity: creating class to hold a fraction as 2/3 and 0.00

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,31 @@
+using System.Collections.Generic;
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+    Fraction fraction1 = new Fraction();
+    Fraction fraction2 = new Fraction(6);
+    Fraction fraction3 = new Fraction(6, 7);
+
+    fraction2.SetTop(5);
+    fraction3.SetTop(3);
+    fraction3.SetBottom(4);
+    Console.WriteLine(fraction1.GetFractionString());
+    Console.WriteLine(fraction1.GetDecimalValue());
+    Console.WriteLine(fraction2.GetFractionString());
+    Console.WriteLine(fraction2.GetDecimalValue());
+    Console.WriteLine(fraction3.GetFractionString());
+    Console.WriteLine(fraction3.GetDecimalValue());
+    fraction3.SetTop(1);
+    fraction3.SetBottom(3);
+    Console.WriteLine(fraction3.GetFractionString());
+    Console.WriteLine(fraction3.GetDecimalValue());
+    
+
     }
 }
